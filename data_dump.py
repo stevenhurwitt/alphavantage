4619b0895d8427af461f32caa3169144fe2662e9
Steven Hurwitt <stevenhurwitt@gmail.com>
2021-04-17 11:32:27 -0500

    manual rebase

--- data_dump.py
+++ data_dump.py
@@ -1,31 +1,46 @@
-from alpha_vantage.async_support.timeseries import TimeSeries
-import matplotlib.pyplot as plt
+from alpha_vantage.timeseries import TimeSeries as TimeSeriesOg
+import multiprocessing
 import datetime as dt
 import pandas as pd
 import numpy as np
-import asyncio
+import json
 import os
 
-api_key = "HXRGE747RBD2KVJ2"
+with open('api_key.json', 'r') as f:
+    api = json.load(f)
+    api_key = api['api_key']
 
-async def get_data(symbol):
-    ts = TimeSeries(key=api_key, output_format='pandas')
+def get_ts(symbol):
     
-    try:
-        data, _ = await ts.get_daily_adjusted(symbol=symbol, outputsize='full')
-        await ts.close()
-        fname = "./data_dump/{}_data.csv".format(symbol)
-        data.to_csv(fname)
+    ts = TimeSeriesOg(key=api_key, output_format='pandas')
+    data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='full')
+    fname = "./data_dump/{}_data.csv".format(symbol)
+    data.to_csv(fname)
 
-    except:
-        pass
-    return(None)
+def download_data(pool_id, symbols):
+    for symbol in symbols:
+        print("[{:02}]: {}".format(pool_id, symbol))
+        get_ts(symbol)
+
 
 if __name__ == "__main__":
 
     nasdaq = pd.read_csv('nasdaq_screener.csv')
-    loop = asyncio.get_event_loop()
-    tasks = [get_data(symbol) for symbol in nasdaq.Symbol]
-    group1 = asyncio.gather(*tasks)
-    results = loop.run_until_complete(group1)
-    print(results)
+    tickers = nasdaq.Symbol
+    tickers = [a for a in tickers if '/' not in a]
+    tickers = [a for a in tickers if '^' not in a]
+    tickers = [a for a in tickers if 'AMP' not in a]
+
+    # PROCESSES = multiprocessing.cpu_count()
+    PROCESSES = 8  # number of parallel process
+    CHUNKS = 6  # one process handle n symbols
+
+    # create a list of n sublist
+    tickers = [tickers[i:i + CHUNKS] for i in range(0, len(tickers), CHUNKS)]
+
+    with multiprocessing.Pool(PROCESSES) as pool:
+<<<<<<< HEAD
+        pool.starmap(download_data, enumerate(tickers, start=1)) 
+=======
+        pool.starmap(download_data, enumerate(tickers, start=1)) 
+>>>>>>> d019f66... git bs
