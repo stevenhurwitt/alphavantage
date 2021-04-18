71762206f3f93ec4a02f74e82f438453a30a96ae
Steven Hurwitt <stevenhurwitt@gmail.com>
2021-04-17 21:34:03 -0500

    calculated years of data per stock

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Helping with a research project that involves downloading all the stock symbols in `nasdaq_screener.csv` using the alpha vantage API. 
 
-The notebook shows how to download the data synchronously, while the `data_dump.py` file will download the data using multiprocessing (unless the API limit gets hit).
+The notebook shows how to download the data synchronously, while the `data_dump.py` file will download the data using multiprocessing (unless the API limit gets hit). `calc_years.py` calculates the years of data available for all stocks. This is then dumped into `year_calc_combined.csv` and appended to `nasdaq_years.csv`.
--- calc_years.py
+++ calc_years.py
@@ -6,21 +6,36 @@ import numpy as np
 import json
 import os
 
-with open('api_key.json', 'r') as f:
-    api = json.load(f)
-    api_key = api['api_key']
-
-def get_ts(symbol):
-    
-    ts = TimeSeries(key=api_key, output_format='pandas')
-    data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='full')
+def get_years(symbol):
     fname = "./data_dump/{}_data.csv".format(symbol)
-    data.to_csv(fname)
+    data = pd.read_csv(fname)
+    data.date = pd.to_datetime(data.date)
+    last_date = np.max(data.date)
+    first_date = np.min(data.date)
+    years = round((last_date - first_date).days / 365,2)
+    return(years)
 
-def download_data(pool_id, symbols):
+def year_calc(pool_id, symbols):
+    years_list = []
+    symbols_list = []
+
     for symbol in symbols:
         print("[{:02}]: {}".format(pool_id, symbol))
-        get_ts(symbol)
+
+        try:
+            years = get_years(symbol)
+            years_list.append(years)
+            symbols_list.append(symbol)
+
+        except:
+            pass
+
+    print('writing data to .csv...')
+    output = dict(zip(symbols_list, years_list))
+    output_df = pd.DataFrame.from_dict(output, orient = 'index')
+    fname = './year_calc/year_calc_{}.csv'.format(pool_id)
+    output_df.to_csv(fname, header = False)
+    print('done.')
 
 
 if __name__ == "__main__":
@@ -39,4 +54,4 @@ if __name__ == "__main__":
     tickers = [tickers[i:i + CHUNKS] for i in range(0, len(tickers), CHUNKS)]
 
     with multiprocessing.Pool(PROCESSES) as pool:
-        pool.starmap(download_data, enumerate(tickers, start=1)) 
+        pool.starmap(year_calc, enumerate(tickers, start=1)) 
